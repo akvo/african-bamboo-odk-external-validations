e3119a21e02f4563efcd689a343106894d626a7f
Iwan Firmawan <firmawaneiwan@gmail.com>
2026-02-11 09:20:02 +0700

    [#12] fix: increase default overlap threshold percentage to 20%
    Stick with 20% thresold at first and once enumerators are more familiar we can be stricter.
    https://akvo.slack.com/archives/C09C8LFCTA5/p1770642858309639?thread_ts=1770642822.353919&cid=C09C8LFCTA5

--- app/src/main/java/org/akvo/afribamodkvalidator/validation/OverlapChecker.kt
+++ app/src/main/java/org/akvo/afribamodkvalidator/validation/OverlapChecker.kt
@@ -131,6 +131,6 @@ class OverlapChecker(
 
     companion object {
         private const val TAG = "OverlapChecker"
-        const val DEFAULT_OVERLAP_THRESHOLD_PERCENT = 5.0
+        const val DEFAULT_OVERLAP_THRESHOLD_PERCENT = 20.0
     }
 }
